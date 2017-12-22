62f25a2bd8495091f3b2d28b776d266db407ed20
Zahin Roja <32425057+zroja@users.noreply.github.com>
2017-12-21 21:15:23 -0500

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -13,33 +13,47 @@ int main()
 
    // Vars
    string sentence;
-   string word;
-
-   int i;
+   string answer;
 
    // Input
    cout << "Welcome to Reverse-the-words" << endl;
-   cout << "Enter your sentence, word, or phrase" << endl;
-   getline(cin,sentence);
 
-        for (int i=0; i < sentence.length()/2; i++)
-{
+        do
+        {
+                cout << "Enter your sentence, word, or phrase" << endl;
+                getline(cin,sentence);
 
-        swap (sentence [i], sentence [sentence.length()-i-1]);
-}
+                int wordbegin =0;
+                for (int i=0; i <= sentence.length(); i++)
+                {
+                        if(!isalpha(sentence[i]))
+                        {
+                                for(int j = wordbegin; j < (i-(i-wordbegin)/2);j++)
+                        {
+                                swap(sentence[j], sentence[ i - j + wordbegin -1]);
+                        }
+                        wordbegin = i+1;
+                        }
+                }
 
    // Output
-   cout << "Your phrase with all of the words reversed is " << endl;
-   cout << sentence << endl ;
+        cout << "Your phrase with all of the words reversed is " << endl;
+        cout << sentence << endl ;
+        cout << "Would you like to reverse the words in another phrase? " << endl;
+        cin >> answer ;
+        cin.ignore();
+
+        } while (answer == "yes");
+
+   cout << "Goodbye" << endl;
 
    return 0;
 }
 
 void swap(char &v1, char &v2)
 {
- char temp;
- temp = v1;
- v1= v2;
- v2=temp;
+        char temp;
+        temp = v1;
+        v1= v2;
+        v2=temp;
 }
-
